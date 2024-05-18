de7f11633d6e9014f8cf19278734a0ff91b282ea
CalebAkomaye <calebakomaye@outlook.com>
2024-05-18 11:12:44 +0100

    group routes added authentication pages and added create ticket functionality,

--- app/loading.jsx
+++ app/loading.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Loading() {
   return (
-    <main className='flex justify-center items-center'>
+    <main className='flex justify-center items-center my-5'>
       <div>
         <h2 className='text-2xl'>Loading...</h2>
         <p>Hopefully not for too long :)</p>
